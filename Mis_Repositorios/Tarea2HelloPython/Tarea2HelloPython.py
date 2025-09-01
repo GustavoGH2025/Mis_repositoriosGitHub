437ca6477863db3b89a2c5c9a540b54b9eed9198
GustavoGH2025 <golivarescas@geociencias.unam.mx>
2025-08-31 23:34:04 -0600

    Agrego archivos reales de Tarea2 y Tarea3

---
+++ Mis_Repositorios/Tarea2HelloPython/Tarea2HelloPython.py
@@ -0,0 +1,46 @@
+
+#Imprimir mensaje
+
+print("hello world")
+
+greetings = "hello world"
+
+print (greetings)
+
+# Condiciones
+
+a = 6
+b = 2
+
+#1
+#Indicacion
+
+print("addition", a + b)
+#suma 8
+
+print("subtraction", a - b)
+#resta 4
+
+print("multiplication", a * b)
+#multiplicacion 12
+
+print("division", a / b)
+#division 3
+__________________________________
+#2
+#Orden directa
+
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+
+#Imprimir
+print(addition)
+#8           
+print(subtraction)          
+#4
+print(multiplication) 
+#12
+print(division)  
+#3
